24b5b56fd459d51f8279d48a8f79f8676150a377
Austin <6193474+axstin@users.noreply.github.com>
2024-09-25 14:00:55 -0400

    Add SYSCALL instruction support

--- plugin.cpp
+++ plugin.cpp
@@ -1,6 +1,8 @@
 #include <hexsuite.hpp>
 #include <intel.hpp>
 
+#include "ntdefs.hpp"
+
 // NT constants.
 //
 static constexpr size_t index_self_ref = 0x1ED;
@@ -307,6 +309,437 @@ hex::microcode_filter mm_dyn_reloc_lifter = [ ] ( codegen_t& cg )
 	}
 	return false;
 };
+
+
+// Lifts SYSCALL.
+//
+
+static bool is_ea_syscall(ea_t ea)
+{
+	insn_t out {};
+	return decode_insn(&out, ea) > 0 && out.itype == NN_syscall;
+}
+
+struct syscall_netnode : netnode
+{
+	union info_t
+	{
+		struct
+		{
+			uint16_t id; // syscall id + 1, else 0
+			uint16_t api_id; // syscall_signatures index + 1, else 0
+		};
+		nodeidx_t value;
+
+		explicit info_t(nodeidx_t value) : value(value) {}
+	};
+
+	syscall_netnode() : netnode("$ ntrays syscall", 0, true) {}
+
+	info_t get_info(ea_t ea) const
+	{
+		return info_t(altval_ea(ea));
+	}
+
+	void set_info(ea_t ea, info_t info)
+	{
+		QASSERT(60801, is_ea_syscall(ea));
+		altset_ea(ea, info.value);
+	}
+
+	uint16_t get_syscall_id(ea_t ea) const
+	{
+		return get_info(ea).id;
+	}
+
+	void set_syscall_id(ea_t ea, uint16_t id)
+	{
+		auto info = get_info(ea);
+		info.id = id;
+		set_info(ea, info);
+	}
+
+	uint16_t get_api_id(ea_t ea) const
+	{
+		return get_info(ea).api_id;
+	}
+
+	void set_api_id(ea_t ea, uint16_t id)
+	{
+		QASSERT(60804, id <= std::size(nt_api_descriptors));
+		auto info = get_info(ea);
+		info.api_id = id;
+		set_info(ea, info); 
+	}
+
+	size_t get_winver()
+	{
+		return altval(0);
+	}
+
+	void set_winver(size_t idx)
+	{
+		QASSERT(60803, idx <= std::size(nt_syscall_maps));
+		altset(0, idx);
+	}
+};
+
+static size_t inf_get_pointer_size()
+{
+	return inf_is_64bit() ? sizeof(uint64_t) : sizeof(uint32_t);
+}
+
+static tinfo_t tinfo_from_argument_descriptor(const nt_api_descriptor::argument_descriptor &descriptor, ea_t ea)
+{
+	tinfo_t tif {};
+	qstring tmp {};
+
+	std::string decl = descriptor.type_name;
+	decl += ";";
+
+	if (parse_decl(&tif, &tmp, nullptr, decl.c_str(), PT_SIL | PT_TYP))
+	{
+		return tif;
+	}
+	else
+	{
+		msg("%zX: could not parse type '%s'\n", ea, descriptor.type_name);
+		return tinfo_t{ inf_is_64bit() ? BT_INT64 : BT_INT32 };
+	}
+}
+
+static hex::call_arg load_stack_value( codegen_t &cg, tinfo_t typ, size_t offset, const char* name )
+{
+	auto ltmp = hex::reg{ cg.mba->alloc_kreg( 8 ), 8 };
+	auto tmp = hex::reg{ cg.mba->alloc_kreg( typ.get_size() ), ( int ) typ.get_size() };
+	cg.mb->insert_into_block( hex::make_add( cg.insn.ea, hex::phys_reg( R_sp, 8 ), hex::operand{ offset, 8 }, ltmp ).release(), cg.mb->tail );
+	cg.mb->insert_into_block( hex::make_ldx( cg.insn.ea, hex::phys_reg( R_ss, 2 ), ltmp, tmp ).release(), cg.mb->tail );
+	return hex::call_arg( tmp, typ, name );
+};
+
+static std::unique_ptr<mcallinfo_t> create_call_info_from_nt_signature(codegen_t &cg, const nt_api_descriptor &descriptor)
+{
+	auto ci = hex::call_info( tinfo_t { BT_INT64 } );
+	QASSERT(60802, inf_is_64bit()); // im lazy
+	for (size_t i = 0; i < descriptor.arguments.size(); i++)
+	{
+		const auto &argument = descriptor.arguments[i];
+		auto type = tinfo_from_argument_descriptor(argument, cg.insn.ea);
+
+		if (i < 4)
+		{
+			size_t regs[4] = { R_r10, R_dx, R_r8, R_r9 };
+			ci->args.push_back(hex::call_arg(hex::phys_reg(regs[i], type.get_size()), type, argument.name ));
+		}
+		else
+		{
+			ci->args.push_back(load_stack_value(cg, type, 8 + i * 8, argument.name));
+		}
+	}
+	return ci;
+}
+
+hex::microcode_filter syscall_lifter = [ ] ( codegen_t& cg )
+{
+	if (cg.insn.itype != NN_syscall)
+		return false;
+
+	if (inf_get_filetype() == f_ELF)
+		return false;
+
+	if (!inf_is_64bit())
+		return false;
+
+	syscall_netnode snn{};
+	const auto info = snn.get_info(cg.insn.ea);
+	const auto winver = snn.get_winver();
+
+	const nt_api_descriptor* signature{};
+
+	// User set API?
+	//
+	if ( info.api_id )
+	{
+		signature = &nt_api_descriptors[info.api_id - 1];
+	}
+	// Syscall ID is known?
+	//
+	else if (info.id && winver)
+	{
+		auto &id_map = nt_syscall_maps[winver - 1].second;
+		if (info.id <= id_map.size())
+		{
+			const auto signature_id = id_map[info.id - 1];
+			if (signature_id)
+			{
+				signature = &nt_api_descriptors[signature_id - 1];
+				msg("%zX: inferred syscall signature %s from ID %u and Windows version '%s'\n", cg.insn.ea, signature->api_name, info.id - 1, nt_syscall_maps[winver - 1].first);
+			}
+		}
+
+		if (!signature)
+			msg("%zX: failed to infer syscall signature from ID %u\n", cg.insn.ea, info.id - 1);
+	}
+
+	const char* helper_name;
+	std::unique_ptr<mcallinfo_t> syscall_ci;
+
+	if (signature)
+	{
+		helper_name = signature->api_name;
+		syscall_ci = create_call_info_from_nt_signature(cg, *signature);
+	}
+	else
+	{
+		if (info.id && !winver) msg("%zX: syscall ID known but no Windows version set\n", cg.insn.ea);
+		helper_name = "__syscall";
+		syscall_ci = hex::call_info(
+			tinfo_t{ BT_INT64 },
+			hex::call_arg( hex::phys_reg( R_ax, 4 ), tinfo_t{ BT_INT32 }, "id" ),
+			hex::call_arg( hex::phys_reg( R_r10, 8 ), tinfo_t{ BT_INT64 }, "arg0" ),
+			hex::call_arg( hex::phys_reg( R_dx, 8 ), tinfo_t{ BT_INT64 }, "arg1" ),
+			hex::call_arg( hex::phys_reg( R_r8, 8 ), tinfo_t{ BT_INT64 }, "arg2" ),
+			hex::call_arg( hex::phys_reg( R_r9, 8 ), tinfo_t{ BT_INT64 }, "arg3" ),
+			load_stack_value( cg, tinfo_t{ BT_INT64 }, 0x28 + 0 * 8, "arg4" ),
+			load_stack_value( cg, tinfo_t{ BT_INT64 }, 0x28 + 1 * 8, "arg5" )
+		);
+	}
+
+	syscall_ci->return_regs.add( reg2mreg( R_ax ), 64 );
+	syscall_ci->spoiled.add( reg2mreg( R_ax ), 64 );
+	syscall_ci->spoiled.add( reg2mreg( R_cx ), 64 );
+	syscall_ci->spoiled.add( reg2mreg( R_r11 ), 64 );
+	// everything else should be spoiled too, not sure if there's a nice way to do this
+	syscall_ci->flags |= FCI_HASCALL;
+
+	auto syscall_call = hex::make_call( cg.insn.ea, hex::helper{ helper_name }, std::move( syscall_ci ) );
+
+	cg.mb->insert_into_block(
+		hex::make_mov( cg.insn.ea, std::move( syscall_call ), hex::phys_reg( R_ax, 8 ) ).release(),
+		cg.mb->tail
+	);
+
+	cg.mb->mark_lists_dirty();
+	return true;
+};
+
+hex::insn_optimizer syscall_optimizer = []( mblock_t *blk, minsn_t *ins, auto )
+{
+	if ( !ins->contains_opcode( m_call ) )
+		return 0;
+
+	auto *call = ins->find_call( true );
+
+	if ( call->is_helper( "__syscall" ) )
+	{
+		QASSERT( 60800, ins->ea != BADADDR && call->d.t == mop_f && !call->d.f->args.empty() );
+		auto *ci = call->d.f;
+		uint64_t syscall_id = 0;
+		if ( ci->args[ 0 ].is_constant( &syscall_id, false ) )
+		{
+			syscall_netnode snn{};
+			auto info = snn.get_info( ins->ea );
+			if ( !info.id )
+			{
+				info.id = syscall_id + 1;
+				snn.set_info( ins->ea, info );
+
+				// TODO: triggering a refresh at this point would be nice
+				msg( "%zX: deduced ID=%u for syscall (maturity=%u), regenerate microcode to update signature\n", ins->ea, syscall_id, blk->mba->maturity );
+			}
+		}
+	}
+
+	return 0;
+};
+
+// used by vdui_t::ctree_to_disasm() in hexx64.dll
+static ea_t get_vdui_ea(vdui_t *vu)
+{
+	vu->get_current_item(USE_KEYBOARD);
+
+	ea_t fict_ea = vu->cfunc->entry_ea;
+
+	if (vu->item.citype == VDI_EXPR)
+	{
+		fict_ea = vu->item.it->ea;
+
+		if (fict_ea == BADADDR)
+		{
+			struct ea_searcher_t : public ctree_visitor_t
+			{
+				const citem_t *result;
+
+				ea_searcher_t(const citem_t *start) : ctree_visitor_t(CV_PARENTS), result(start) {}
+
+				int visit_item(citem_t *item)
+				{
+					if (item != result) return 0;
+					for (size_t i = parents.size(); i-- > 0;)
+					{
+						if (parents[i]->ea != BADADDR)
+						{
+							result = parents[i];
+							break;
+						}
+					}
+					return 1;
+				}
+
+				int idaapi visit_insn(cinsn_t *insn) override { return visit_item(insn); }
+  				int idaapi visit_expr(cexpr_t *expr) override { return visit_item(expr); }
+			};
+
+			ea_searcher_t searcher(vu->item.it);
+			searcher.apply_to(&vu->cfunc->body, nullptr);
+			fict_ea = searcher.result->ea;
+		}
+	}
+	else if (vu->tail.citype == VDI_TAIL)
+	{
+		fict_ea = vu->tail.loc.ea;
+	}
+	else if (vu->head.citype == VDI_TAIL)
+	{
+		fict_ea = vu->head.loc.ea;
+	}
+
+	return vu->mba->map_fict_ea(fict_ea);
+}
+
+struct api_chooser_t : chooser_t
+{
+	static constexpr int WIDTHS[] = { 30, 50 };
+	static constexpr const char *COLUMNS[] = { "Name", "Arguments" };
+
+	api_chooser_t() : chooser_t(CH_KEEP | CH_MODAL, std::size(WIDTHS), WIDTHS, COLUMNS, "Select an API", 0) {}
+	
+	void idaapi get_row(qstrvec_t *out, int *out_icon, chooser_item_attrs_t *out_attrs, size_t n) const override
+	{
+		if (n == 0)
+		{
+			(*out)[0] = "Automatic";
+		}
+		else
+		{
+			(*out)[0] = nt_api_descriptors[n - 1].api_name;
+			(*out)[1] = nt_api_descriptors[n - 1].arguments_to_string().c_str();
+		}
+	}
+
+	size_t idaapi get_count() const override
+	{
+		return std::size(nt_api_descriptors) + 1;
+	}
+};
+
+struct winver_chooser_t : chooser_t {
+	static constexpr int WIDTHS[] = { 50 };
+	static constexpr const char *COLUMNS[] = { "Version" };
+
+	winver_chooser_t() : chooser_t(CH_KEEP | CH_MODAL, std::size(WIDTHS), WIDTHS, COLUMNS, "Select a Windows Version", 0) {}
+	
+	void idaapi get_row(qstrvec_t *out, int *out_icon, chooser_item_attrs_t *out_attrs, size_t n) const override
+	{
+		if (n == 0)
+		{
+			(*out)[0] = "Disable automatic deduction";
+		}
+		else
+		{
+			(*out)[0] = nt_syscall_maps[n - 1].first;
+		}
+	}
+
+	size_t idaapi get_count() const override
+	{
+		return std::size(nt_syscall_maps) + 1;
+	}
+};
+
+static bool idaapi menu_set_syscall_api(vdui_t *vdui)
+{
+	syscall_netnode snn {};
+	auto ea = get_vdui_ea(vdui);
+	auto info = snn.get_info(ea);
+
+	api_chooser_t chooser {};
+	auto choice = chooser.choose(info.api_id);
+
+	if (choice >= 0)
+	{
+		info.api_id = choice;
+		snn.set_info(ea, info);
+		vdui->refresh_view(true);
+	}
+
+	return false;
+}
+
+static bool idaapi menu_set_winver(vdui_t *vdui)
+{
+	syscall_netnode snn {};
+
+	winver_chooser_t chooser {};
+	auto choice = chooser.choose(snn.get_winver());
+
+	if (choice >= 0)
+	{
+		syscall_netnode().set_winver(choice);
+		vdui->refresh_view(true);
+	}
+}
+
+namespace
+{
+	class menu_action_handler : public action_handler_t
+	{
+	public:
+		typedef std::function<bool(vdui_t *)> handler_t;
+		bool is_enabled;
+
+		explicit menu_action_handler(handler_t handler)
+			: is_enabled(true), handler(std::move(handler))
+		{
+		}
+
+		menu_action_handler(handler_t handler, const bool enabled)
+			: is_enabled(enabled), handler(std::move(handler))
+		{
+		}
+
+		int idaapi activate(action_activation_ctx_t* ctx) override
+		{
+			const auto vdui = get_widget_vdui(ctx->widget);
+			return handler(vdui) ? 1 : 0;
+		}
+
+		action_state_t idaapi update(action_update_ctx_t* ctx) override
+		{
+			return ctx->widget_type == BWN_PSEUDOCODE ? AST_ENABLE_FOR_WIDGET : AST_DISABLE_FOR_WIDGET;
+		}
+
+	private:
+		handler_t handler;
+	};
+
+	static menu_action_handler set_syscall_api_handler { menu_set_syscall_api };
+	static menu_action_handler set_winver_handler { menu_set_winver };
+
+	static action_desc_t action_descs[] = {
+		ACTION_DESC_LITERAL("ntrays:set_syscall_api", "Set syscall API", &set_syscall_api_handler, nullptr, nullptr, -1),
+		ACTION_DESC_LITERAL("ntrays:set_winver", "Set Windows version", &set_winver_handler, nullptr, nullptr, -1)
+	};
+}
+
+hex::hexrays_callback hexrays_popup_event = hex::hexrays_callback_for<hxe_populating_popup>([](TWidget *widget, TPopupMenu *popup_menu, vdui_t *vu)
+{
+	if (is_ea_syscall(get_vdui_ea(vu)))
+		attach_action_to_popup(vu->ct, popup_menu, "ntrays:set_syscall_api");
+	attach_action_to_popup(vu->ct, popup_menu, "ntrays:set_winver");
+
+	return 0;
+});
 
 // Lifts CPUID.
 //
@@ -727,24 +1160,15 @@ hex::microcode_filter iretq_lifter = [ ] ( codegen_t& cg )
 	if ( cg.insn.itype != NN_iretq )
 		return false;
 
-	auto load_at = [ & ] ( tinfo_t typ, size_t offset, const char* name )
-	{
-		auto ltmp = hex::reg{ cg.mba->alloc_kreg( 8 ), 8 };
-		auto tmp = hex::reg{ cg.mba->alloc_kreg( typ.get_size() ), ( int ) typ.get_size() };
-		cg.mb->insert_into_block( hex::make_add( cg.insn.ea, hex::phys_reg( R_sp, 8 ), hex::operand{ offset, 8 }, ltmp ).release(), cg.mb->tail );
-		cg.mb->insert_into_block( hex::make_ldx( cg.insn.ea, hex::phys_reg( R_ss, 2 ), ltmp, tmp ).release(), cg.mb->tail );
-		return hex::call_arg( tmp, typ, name );
-	};
-
 	tinfo_t vptr{};
 	vptr.create_ptr( tinfo_t{ BT_VOID } );
 	auto ci = hex::call_info(
 		tinfo_t{ BT_VOID },
-		load_at( vptr, 0, "ip" ),
-		load_at( tinfo_t{ BT_INT16 }, 8, "cs" ),
-		load_at( tinfo_t{ BT_INT32 }, 0x10, "flags" ),
-		load_at( vptr, 0x18, "sp" ),
-		load_at( tinfo_t{ BT_INT16 }, 0x20, "ss" )
+		load_stack_value( cg, vptr, 0, "ip" ),
+		load_stack_value( cg, tinfo_t{ BT_INT16 }, 8, "cs" ),
+		load_stack_value( cg, tinfo_t{ BT_INT32 }, 0x10, "flags" ),
+		load_stack_value( cg, vptr, 0x18, "sp" ),
+		load_stack_value( cg, tinfo_t{ BT_INT16 }, 0x20, "ss" )
 	);
 	ci->flags |= FCI_NORET;
 	cg.mb->insert_into_block( hex::make_call( cg.insn.ea, hex::helper( "__iretq" ), std::move( ci ) ).release(), cg.mb->tail );
@@ -1018,7 +1442,10 @@ constexpr hex::component* component_list[] = {
 	&iretq_lifter,                &sysretq_lifter,
 	&type_enforcer,               &rdrand_rdseed_lifter,
 	&rdssp_lifter,                &vmread_lifter,
-	&nt_assert_lifter
+	&nt_assert_lifter,
+
+	&syscall_lifter,              &syscall_optimizer,
+	&hexrays_popup_event
 };
 
 // Plugin declaration.
@@ -1031,11 +1458,23 @@ struct ntrays : plugmod_t
 	void set_state( bool s ) 
 	{
 		if ( s )
-			remove_rsb_flush(), create_kuser_seg(), fix_udts();
+		{
+			remove_rsb_flush();
+			create_kuser_seg();
+			fix_udts();
+
+			for ( const auto &desc : action_descs )
+				register_action( desc );
+		}
+		else
+		{
+			for ( const auto &desc : action_descs )
+				unregister_action(desc.name);
+		}
 		components.set_state( s );
 	}
 	ntrays() { set_state( nn.altval( 0 ) == 0 ); }
-	~ntrays() { components.uninstall(); }
+	~ntrays() { set_state(false); components.uninstall(); }
 
 	bool run( size_t ) override
 	{
